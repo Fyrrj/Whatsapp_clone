47fc0b86c808ec773abe35beefe5088c8a1f4adf
Klaudiusz Florek <florekklaudiusz@gmail.com>
2020-08-24 18:58:30 +0200

    80% front end

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBPFxhgZGGnqQhKFJMvyP09vmyn9I8hx9w",
+  authDomain: "whatsappclone-eff66.firebaseapp.com",
+  databaseURL: "https://whatsappclone-eff66.firebaseio.com",
+  projectId: "whatsappclone-eff66",
+  storageBucket: "whatsappclone-eff66.appspot.com",
+  messagingSenderId: "230546576517",
+  appId: "1:230546576517:web:b14eb1ddcced80eb48c5ec",
+  measurementId: "G-HPBS3THWXM",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ src/components/SidebarChat.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { Avatar } from "@material-ui/core";
+import "../SidebarChat.css";
+
+function SidebarChat({ addNewChat }) {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat");
+
+    if (roomName) {
+      //...
+    }
+  };
+
+  return (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarChat;
